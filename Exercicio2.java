a4a56c2c14f0c9f311ecae8509d3a87f7ed4e58d
Alef Cezario <161400415+alefcezario@users.noreply.github.com>
2024-03-11 21:00:06 -0300

    Create Exercicio2.java

---
+++ Exercicio2.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio2;
+
+/**
+ *
+ * @author aluno
+ */
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+
+       double valorEtanol;
+       double valorGasolina;
+       double setentaPorcentoGasolina;
+       
+              
+       Scanner entrada = new Scanner(System.in);
+      
+         System.out.println("Digite o preço do etanol: ");
+  
+         valorEtanol = entrada.nextInt();
+         
+         System.out.println("Agora o preço da gasolina: ");
+         valorGasolina = entrada.nextInt();
+         
+         setentaPorcentoGasolina = valorGasolina/100*70;
+         
+        if (valorEtanol<setentaPorcentoGasolina ){
+            
+         System.out.println("Está valendo a pena abastecer com Etanol! ");
+         
+        }else{
+         System.out.println("Está valendo a pena abastecer com Gasolina! ");   
+        }
+         
+         entrada.close();
+    }
+    
+}
